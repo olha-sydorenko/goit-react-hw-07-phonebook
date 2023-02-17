acfc10940353c47dc70e10b073e4a9b7bec7b7b4
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-02-17 14:52:47 +0200

    Update Filter.styled.js

--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -2,10 +2,12 @@ import styled from 'styled-components';
 
 export const FilterText = styled.p`
   font-size: 18px;
+  margin-bottom: 8px;
 `;
 
 export const Input = styled.input`
   padding-left: 20px;
+  margin-bottom: 20px;
   width: 200px;
   height: 20px;
   border: 1px solid grey;
