f6cebe30b190d7ccedf099f34817b1f9cfd6408b
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-02-23 15:29:14 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,30 +6,21 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { Container } from './Container/Container';
 
-export const App = () => {
-  const currentContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
+const currentContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
+export const App = () => {
   const getCurrentContacts = () => {
     return JSON.parse(localStorage.getItem('contactsBase')) ?? currentContacts;
   };
   const [contacts, setContacts] = useState(getCurrentContacts);
   const [filter, setFilter] = useState('');
 
-  // componentDidMount() {
-  //     if (JSON.parse(localStorage.getItem('contactsBase'))) {
-  //       this.setState({
-  //         contacts: JSON.parse(localStorage.getItem('contactsBase')),
-  //       });
-  //     }
-  //   }
-
   useEffect(() => {
-    // if (!contacts) return;
     localStorage.setItem('contactsBase', JSON.stringify(contacts));
   }, [contacts]);
   console.log(contacts);
@@ -51,8 +42,6 @@ export const App = () => {
     }
 
     setContacts(contacts => [...contacts, contact]);
-    console.log('new', contacts);
-    console.log(name.toLowerCase());
   };
 
   const deleteContact = contactId => {
